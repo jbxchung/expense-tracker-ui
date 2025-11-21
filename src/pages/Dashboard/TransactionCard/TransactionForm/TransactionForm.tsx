27865c770447147355d71aab5d6e44ab5e42ff96
Brandon Chung <jbx.chung@gmail.com>
2025-11-21 12:25:50 -0500

    add file upload button

--- src/pages/Dashboard/TransactionCard/TransactionForm/TransactionForm.tsx
+++ src/pages/Dashboard/TransactionCard/TransactionForm/TransactionForm.tsx
@@ -1,12 +1,33 @@
-import { useCallback, useMemo, useState, type FC } from 'react';
+import { useCallback, useMemo, useRef, useState, type FC } from 'react';
 
 import styles from './TransactionForm.module.scss';
+import Button, { ButtonVariants } from 'components/Button/Button';
 
 
 const TransactionForm: FC = () => {
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+
+  const handleFileUpload = (e: any) => {
+    const files = e.target.files;
+    if (!files || files.length === 0) {
+      setUploadedFile(null);
+      return;
+    }
+
+    const file = files[0];
+    setUploadedFile(file);
+    console.log("Selected file:", file);
+  }
+
   return (
     <div>
-      todo  
+      <div className={styles.uploadFile}>
+        <input type="file" onChange={handleFileUpload} ref={fileInput}/>
+        <Button variant={ButtonVariants.PRIMARY} onClick={() => fileInput.current?.click()}>Upload File</Button>
+        <span>{uploadedFile?.name}</span>
+      </div>
     </div>
   );
 };
